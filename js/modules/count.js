62b4ce4cd2c168612a6a11311d1f121099e6c543
rokas <rokasra@gmail.com>
2022-03-31 14:37:36 +0300

    added btn

---
+++ js/modules/count.js
@@ -0,0 +1,7 @@
+export default function makeDoubleAndPlus(num) {
+  const double = num * 2;
+  const plus10 = double + 10;
+  return plus10;
+}
+
+export const colors = ["red", "green", "blue"];
---
+++ js/modules/data.js
@@ -0,0 +1,2 @@
+export const p =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. At, magnam dolor eos itaque fuga eligendi nihil dolorum voluptas iste quam, officiis soluta in autem provident labore praesentium totam iusto excepturi!. <button> click me </button>";
